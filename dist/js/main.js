3d9cfecd8f11171f67eb7b24989a8cec1164e18f
CROGOT <48329827+CROGOT@users.noreply.github.com>
2022-02-08 14:01:37 +0300

    5 день

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
-// Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
+
 (function () {
   "use strict";
-
+  // Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
   // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
   var forms = document.querySelectorAll(".needs-validation");
 
@@ -20,4 +20,15 @@
       false
     );
   });
+
+  // именение размера родителя в зависимости от потомка
+  const coordinates__wrap_content=document.querySelector('#resized_block');
+  const coordinates__wrap=document.querySelector('.coordinates__wrap');
+  console.log(coordinates__wrap_content);
+  const resize_coordinates_div = () => {
+    coordinates__wrap.style.height=coordinates__wrap_content.offsetHeight + 'px';
+  }
+  resize_coordinates_div();
+  window.addEventListener(`resize`, resize_coordinates_div, false);
+
 })();
